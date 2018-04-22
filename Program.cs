944c429a67a31f6fd03463fcce5f0f0edc4e0ea5
Elili <1364069139@qq.com>
2018-04-22 22:53:44 +0800

    添加几个名词

--- Program.cs
+++ Program.cs
@@ -2,12 +2,19 @@
 
 namespace Test
 {
+    /*
+    1.CIL：JIT
+    2.程序集：包括可执行的应用程序文件
+    3.托管代码：
+    4.垃圾回收（GC）
+    */
     class Program
     {
         static void Main(string[] args)
         {
             //这是第一个c#程序
             Console.WriteLine("Hello World!");
+            //Console.ReadKey();
         }
     }
 }
